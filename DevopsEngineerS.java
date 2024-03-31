abe1eb725a6f33b48dd03bec513794f39d45324c
01fe22bca138 <01fe22bca138@kletech.ac.in>
2024-03-31 18:37:37 +0530

    code to calculate bonus

---
+++ DevopsEngineerS.java
@@ -0,0 +1,12 @@
+public class DevopsEngineerS  {
+    float bonus=1000;
+     
+    public void displaySalary(float salary)
+    {
+     salary+=bonus;
+     System.out.println("Total salary:"+salary);
+    }
+     
+     }
+ 
+ 
